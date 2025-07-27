1c8fc772c9a59e09f0b5475a0c285a1942dba57b
Hema-96 <hema@agilecyber.com>
2025-07-27 21:01:50 +0530

    path change

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("/render/secrets/firebase-adminsdk.json")
+cred = credentials.Certificate("/etc/secrets/firebase-adminsdk.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
